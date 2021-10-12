bd57f4a73e657478383d14f90f7b3aec8a61c458
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-10-12 11:19:05 -0400

    Axios apdate

--- src/components/utils/Axios.js
+++ src/components/utils/Axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 const Axios = axios.create({
     baseURL:process.env.NODE_ENV === "development"
-    ?"http://localhost:8080"
-    :"bocure.kvaknastya.com",
+    ?"http://localhost:8080/api"
+    :"/api",
     timeout:50000
 })
 
